e8248bbfa748c7c3b0b023de4c8e9e54f8f32e71
Kris Patel <kpatel29@binghamton.edu>
2024-11-18 17:53:52 -0500

    updating

---
+++ src/lives.py
@@ -0,0 +1,10 @@
+class Lives:
+    def __init__(self, lives=3, x=[], y=[], image_file):
+        self.lives = lives
+        self.x = x
+        self.y = y
+        self.image = image_file
+    def deduct(self):
+        pass
+    def game_over(self):
+        pass
---
+++ src/score.py
@@ -0,0 +1,3 @@
+class Score:
+    def __init__(self, score=0):
+        self.score = score
